05126c1f0949c02c5c524a59f9e426095c69204b
crazyyi <gdzack@gmail.com>
2016-03-02 23:21:58 +0800

    Use correct Associations

--- db/migrate/20160302144537_create_wellspring_taggings.rb
+++ db/migrate/20160302144537_create_wellspring_taggings.rb
@@ -1,8 +1,8 @@
 class CreateWellspringTaggings < ActiveRecord::Migration
   def change
     create_table :wellspring_taggings do |t|
-      t.belongs_to :wellspring_entry, index: true, foreign_key: true
-      t.belongs_to :wellspring_tag, index: true, foreign_key: true
+      t.belongs_to :entry, index: true, foreign_key: true
+      t.belongs_to :tag, index: true, foreign_key: true
 
       t.timestamps null: false
     end
