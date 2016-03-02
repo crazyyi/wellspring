75357c0f76cb73579debd763eeb1b9f0d20fc734
crazyyi <gdzack@gmail.com>
2016-03-02 23:48:33 +0800

    little experiment with table associations

--- app/models/wellspring/tag.rb
+++ app/models/wellspring/tag.rb
@@ -1,6 +1,6 @@
 module Wellspring
   class Tag < ActiveRecord::Base
   	has_many :taggings
-	has_many :entries, through: :taggings
+	has_many :entries, through: :taggings, class_name: "Wellspring:Entry"
   end
 end
